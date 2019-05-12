cd53db7f5c723a1f43f5b3e5e877eeb63e6f8a82
Matheus Mota <smota.matheus@gmail.com>
2019-05-12 00:00:31 -0300

    final cloud setup - new domain

--- setup.sh
+++ setup.sh
@@ -5,31 +5,35 @@ sudo cp ./configs/apache2/000-default.conf /etc/apache2/sites-available/000-defa
 sudo cp ./configs/apache2/index.html /var/www/index.html 
 sudo systemctl restart apache2
 
-#sudo docker-compose up -d
+sudo docker-compose up -d
 
 sudo rm repositories -r
 mkdir   repositories  
 cd      repositories 
 
+
+
 git clone https://github.com/datasci4health/harena-space
 cd  harena-space
-sudo docker-compose up -d
+sudo docker-compose -f docker-compose.yml -f ../../resources/docker/harena-space/docker-compose.yaml  up -d --build
 cd ..
 
 
 git clone https://github.com/datasci4health/harena-manager
 cd  harena-manager
-sudo docker-compose up -d  harena-manager-database
-sudo docker-compose run harena-manager adonis migration:run --force
+sudo docker-compose up -d --build
+sudo docker-compose down
+sudo docker-compose run harena-manager adonis migration:run --force 
 sudo docker-compose run harena-manager adonis seed          --force
-sudo docker-compose up -d
+sudo docker-compose down
+sudo docker-compose up -d 
 cd ..
 
 
 git clone https://github.com/datasci4health/harena-logger
 cd  harena-logger
-sudo docker-compose up -d
-cd ..
+sudo docker-compose up -d --build
+cd .. 
 
 
 cd ..
