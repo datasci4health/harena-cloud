25aeb3cf4c5fe4b028e95f2a37575ed68f867ccc
Matheus Mota <smota.matheus@gmail.com>
2020-10-18 23:32:38 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,16 +1,22 @@
 sudo apt install -y htop vim curl wget docker-compose
 
-#sudo apt install -y apache2 php 
-#sudo a2enmod proxy_http
-#sudo rm /var/www/html/index.html 
-#sudo cp ./configs/apache2/000-default.conf /etc/apache2/sites-available/000-default.conf
-#sudo cp ./configs/apache2/index.html /var/www/index.html 
-#sudo apache2ctl configtest
-#sudo systemctl restart apache2
+sudo apt install -y apache2 php 
+sudo a2enmod proxy_http
+sudo rm /var/www/html/index.html 
+sudo cp ./configs/apache2/000-default.conf /etc/apache2/sites-available/000-default.conf
+sudo cp ./configs/apache2/index.html /var/www/index.html 
+sudo apache2ctl configtest
+sudo systemctl restart apache2
 
-#sudo apt install -y python-certbot-apache
-# sudo certbot --apache -d harena.ds4h.org
-# sudo certbot renew --dry-run
+#sudo apt install -y certbot python-certbot-apache 
+
+
+sudo snap install core; sudo snap refresh core
+sudo snap install --classic certbot
+sudo ln -s /snap/bin/certbot /usr/bin/certbot
+sudo certbot --apache
+sudo certbot --apache -d harena.ds4h.org
+sudo certbot renew --dry-run
 
 
 sudo docker-compose pull
